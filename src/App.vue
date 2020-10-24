5d4d0656103e3a51a8545937e6968b1cbcf5dad9
Joakim <jockeeee.s@gmail.com>
2020-10-24 15:30:15 +0200

    lite vue grejs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
   <div id="app">
-    
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
+    Header,
+    Todos,
+    AddTodo
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title:  "Att göra 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title:  "Att göra 2",
+          completed:  false
+        },
+        {
+          id: 3,
+          title:  "Att göra 3",
+          completed:  false
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todo = [...this.todos, newTodo]
+    }
   }
 }
 </script>
@@ -24,5 +60,18 @@ export default {
   body {
     font-family: Arial, Arial, Helvetica, sans-serif;
     line-height: 1.4;
+  }
+
+  .btn {
+    display: inline-block;
+    border: none;
+    background: #555;
+    color: #fff;
+    padding: 7px 20px;
+    cursor: pointer;
+  }
+
+  .btn:hover {
+    background: #666;
   }
 </style>
